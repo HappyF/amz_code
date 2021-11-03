8bc0b25658cc14a926e9922e9a77d906419cc1ba
happyf <shxpdfl@126.com>
2021-11-03 11:40:55 +0800

    123

--- bsr_etl.py
+++ bsr_etl.py
@@ -30,10 +30,10 @@ def get_reviews(n):
     
 def get_stars(n):
     #n=n.replace('','0.0')
-    return n[:18].replace(' out of 5 stars','')
-
-# 日期问题
-
+    try:
+        return n[:18].replace(' out of 5 stars','')
+    except:
+        return 0
 
 
 # 抓取数据初步处理
@@ -44,25 +44,24 @@ def clean_df(df,file):
     df_filter['stars']=df['stars'].apply(get_stars)
     df_filter['reviews']=df['reviews'].apply(get_reviews)
     #
-    df_filter['price']=df['price'].apply(lambda x:x.replace('$','').replace(',',''))
+    df_filter['price']=df['price'].apply(lambda x:str(x).replace('$','').replace(',',''))
     df_filter['rank_big']=df['rank_big'].apply(lambda x: x.replace('#',''))
     df_filter['rank_small']=df['rank_small'].apply(lambda x: x.replace('#',''))
 
     # 抓取日期，类目
     df_filter['insert_date']=dt.datetime.now().strftime("%Y-%m-%d")
-    df_filter['type']=file.split('-')[0].replace(PATH,'')
-    df_filter['date']=file.split('-')[2].replace('.csv','')
-    df_filter['class']=file.split('-')[1]
+    # df_filter['type']=file.split('-')[0].replace(PATH,'')
+    df_filter['date']=file.split('-')[1].replace('.csv','')
+    df_filter['class']=file.split('-')[0]
     return df_filter
     
-
-
 # 如果通过测试，放到数据库中
 def put_database(df):
     conn=pymysql.connect(host='rm-m5ex5024i8851023yqo.mysql.rds.aliyuncs.com',user='amz1',password='amz123456+',database='amz_data')
     cur=conn.cursor()
     for i in tqdm(range(len(df))):
-        sql="insert into listdb(rank_small,title,url,reviews_url,stars,reviews,sell,rank_big,rank_big_name,asin,brand,on_shelf,buy_limit,price,insert_date,type,date,class) values('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(df.loc[i,'rank_small'],df.loc[i,'title'],df.loc[i,'url'],df.loc[i,'reviews_url'],df.loc[i,'stars'],str(df.loc[i,'reviews']), df.loc[i,'sell'],df.loc[i,'rank_big'],df.loc[i,'rank_big_name'],df.loc[i,'asin'],df.loc[i,'brand'],df.loc[i,'on_shelf'],df.loc[i,'buy_limit'],df.loc[i,'price'],df.loc[i,'insert_date'],df.loc[i,'type'],df.loc[i,'date'],df.loc[i,'class'])
+        sql="insert into listdb(rank_small,title,url,reviews_url,stars,reviews,sell,rank_big,rank_big_name,asin,brand,on_shelf,buy_limit,price,insert_date,type,date,class) values('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(df.loc[i,'rank_small'],df.loc[i,'title'].replace("'","\'"),df.loc[i,'url'],df.loc[i,'reviews_url'],df.loc[i,'stars'],str(df.loc[i,'reviews']), df.loc[i,'sell'],df.loc[i,'rank_big'],df.loc[i,'rank_big_name'],df.loc[i,'asin'],df.loc[i,'brand'].replace(".","\'"),df.loc[i,'on_shelf'],df.loc[i,'buy_limit'],df.loc[i,'price'],df.loc[i,'insert_date'],df.loc[i,'type'],df.loc[i,'date'],df.loc[i,'class'])
+        print(sql)
         cur.execute(sql)
     conn.commit()
     cur.close()
@@ -73,8 +72,9 @@ def main():
         for file in file_list:
             #try:
             df=pd.read_csv(file)
+            #return df
             df_final=clean_df(df,file)
-            df_final.to_csv(SAVE_PATH+file.split('-')[0].replace(PATH,'')+'.csv')
+            df_final.to_csv(SAVE_PATH+file.replace(PATH,''))
             print('======存入数据库=====')
             put_database(df_final)
             
--- reviews_p1.py
+++ reviews_p1.py
@@ -10,9 +10,13 @@ import pymysql
 import pandas as pd
 import re
 
+cls='monitor' # 所属产品
+ 
 # 从数据库中获取各自的类目
 conn=pymysql.connect(host='rm-m5ex5024i8851023yqo.mysql.rds.aliyuncs.com',user='amz1',password='amz123456+',database='amz_data')
 cur=conn.cursor()
 
+# 先选择产品，再排重
+sql="select asin,reviews from  where class='{}'".format(cls)
 
 # 
