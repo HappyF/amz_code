5da809175fbb0d493d0f73eaf3086e50bee01095
happyf <shxpdfl@126.com>
2021-11-07 13:43:01 +0800

    add reviews

--- bsr_etl.py
+++ bsr_etl.py
@@ -15,8 +15,9 @@ from tqdm import tqdm
 
 
 # 数据录入，确定导入哪些
-PATH='/Users/happyf/Desktop/amazon/d1/' # 数据所在
-SAVE_PATH='/Users/happyf/Desktop/amazon/d1/save/'
+PATH='E:/Desktop/新建文件夹/整理/' # 数据所在
+SAVE_PATH='E:/Desktop/新建文件夹/保存/'
+# 如果保存的是csv，下面改成csv
 file_list=glob.glob(PATH+'*.csv')
 
 
@@ -35,7 +36,6 @@ def get_stars(n):
     except:
         return 0
 
-
 # 抓取数据初步处理
 def clean_df(df,file):
     # 把nan数据转换成‘’
@@ -44,15 +44,15 @@ def clean_df(df,file):
     df_filter['stars']=df['stars'].apply(get_stars)
     df_filter['reviews']=df['reviews'].apply(get_reviews)
     #
-    df_filter['price']=df['price'].apply(lambda x:str(x).replace('$','').replace(',',''))
+    df_filter['price']=df['price'].fillna('0').apply(lambda x:x.replace('$','').replace(',',''))
     df_filter['rank_big']=df['rank_big'].apply(lambda x: x.replace('#',''))
     df_filter['rank_small']=df['rank_small'].apply(lambda x: x.replace('#',''))
 
     # 抓取日期，类目
     df_filter['insert_date']=dt.datetime.now().strftime("%Y-%m-%d")
     # df_filter['type']=file.split('-')[0].replace(PATH,'')
-    df_filter['date']=file.split('-')[1].replace('.csv','')
-    df_filter['class']=file.split('-')[0]
+    df_filter['date']=pd.to_datetime(df_filter['date'])
+    #df_filter['class']=file.split('-')[0]
     return df_filter
     
 # 如果通过测试，放到数据库中
@@ -60,24 +60,27 @@ def put_database(df):
     conn=pymysql.connect(host='rm-m5ex5024i8851023yqo.mysql.rds.aliyuncs.com',user='amz1',password='amz123456+',database='amz_data')
     cur=conn.cursor()
     for i in tqdm(range(len(df))):
-        sql="insert into listdb(rank_small,title,url,reviews_url,stars,reviews,sell,rank_big,rank_big_name,asin,brand,on_shelf,buy_limit,price,insert_date,type,date,class) values('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(df.loc[i,'rank_small'],df.loc[i,'title'].replace("'","\'"),df.loc[i,'url'],df.loc[i,'reviews_url'],df.loc[i,'stars'],str(df.loc[i,'reviews']), df.loc[i,'sell'],df.loc[i,'rank_big'],df.loc[i,'rank_big_name'],df.loc[i,'asin'],df.loc[i,'brand'].replace(".","\'"),df.loc[i,'on_shelf'],df.loc[i,'buy_limit'],df.loc[i,'price'],df.loc[i,'insert_date'],df.loc[i,'type'],df.loc[i,'date'],df.loc[i,'class'])
-        print(sql)
+        sql="insert into listdb(rank_small,title,url,reviews_url,stars,reviews,sell,rank_big,rank_big_name,asin,brand,on_shelf,buy_limit,price,insert_date,type,date,class) values('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')".format(df.loc[i,'rank_small'],df.loc[i,'title'].replace("'","\'"),df.loc[i,'url'],df.loc[i,'reviews_url'],df.loc[i,'stars'],str(df.loc[i,'reviews']), df.loc[i,'sell'],df.loc[i,'rank_big'],df.loc[i,'rank_big_name'],df.loc[i,'asin'],df.loc[i,'brand'].replace(".","\'"),df.loc[i,'on_shelf'].replace('?',''),df.loc[i,'buy_limit'],df.loc[i,'price'],df.loc[i,'insert_date'],df.loc[i,'type'],df.loc[i,'date'],df.loc[i,'class'])
         cur.execute(sql)
     conn.commit()
     cur.close()
     conn.close()
 
+
+
 def main():
+    
     if file_list:
         for file in file_list:
             #try:
-            df=pd.read_csv(file)
+            df=pd.read_csv(file,encoding='gbk')
             #return df
             df_final=clean_df(df,file)
-            df_final.to_csv(SAVE_PATH+file.replace(PATH,''))
+            
+            df_final.to_csv(SAVE_PATH+file.split('\\')[1])
             print('======存入数据库=====')
             put_database(df_final)
-            
+            return df_final
             #except:
              #   continue
     else:
@@ -85,4 +88,4 @@ def main():
         
         
 if __name__ == '__main__':
-    main()
+    aaa=main()
--- reviews_p1.py
+++ reviews_p1.py
@@ -8,15 +8,61 @@ Created on Tue Nov  2 18:53:04 2021
 
 import pymysql
 import pandas as pd
-import re
+from tqdm import tqdm
 
-cls='monitor' # 所属产品
- 
+cls='E:learningamazonlistmonitors' # 所属产品
+n=3 # 多少天
+
 # 从数据库中获取各自的类目
-conn=pymysql.connect(host='rm-m5ex5024i8851023yqo.mysql.rds.aliyuncs.com',user='amz1',password='amz123456+',database='amz_data')
-cur=conn.cursor()
+def get_data_from_mysql(sql):
+        conn = pymysql.connect(host='rm-m5ex5024i8851023yqo.mysql.rds.aliyuncs.com',user='amz1',password='amz123456+',database='amz_data')
+        cursor = conn.cursor()
 
-# 先选择产品，再排重
-sql="select asin,reviews from  where class='{}'".format(cls)
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        return data
 
-# 
+# 先选择产品，再排重，下面SQL的逻辑，是选取每个asin中最近一次抓取时的asin
+sql="""
+-- select t1.date, t2.asin,t1.rank_big_mean,t1.rank_small_mean,t2.reviews
+select t2.asin,t2.reviews
+from
+(select max(date) date,asin,round(avg(rank_big),0) rank_big_mean, round(avg(rank_small),0) rank_small_mean,count(asin) as n from listdb 
+where class="{}" 
+group by asin) t1
+left JOIN listdb t2
+on t1.date=t2.date and t1.asin=t2.asin
+where t1.n>={} and t2.reviews>0
+""".format(cls,n)
+
+#
+df_reviews=pd.DataFrame(get_data_from_mysql(sql),columns=['asin','reviews'])
+df_reviews['reviews']=df_reviews['reviews'].apply(lambda x:int(x.replace('.0','')))
+
+base_url='https://www.amazon.com/product-reviews/'
+reviews_p1_url=[]
+# 进行判断
+# 如果小于10条，直接全抓，无论怎么样都是这些
+url_less=df_reviews[df_reviews.reviews<=10]['asin']
+for asin in url_less:
+    reviews_p1_url.append(base_url+asin+'?currency=USD&language=en_US&sortBy=helpful&pageNumber=')
+
+# 如果大于10条，分开抓，最后聚合
+url_more=df_reviews[df_reviews.reviews>10]['asin']
+info=[
+        ('Image and video',"?currency=USD&language=en_US&sortBy=helpful&mediaType=media_reviews_only&pageNumber="), # 全部
+        ('Critical','?currency=USD&language=en_US&sortBy=helpful&filterByStar=critical&pageNumber='), # 250//10
+        ('Recent','?currency=USD&language=en_US&sortBy=recent&reviewerType=all_reviews&pageNumber='), # 500
+        ('Positive','?currency=USD&language=en_US&sortBy=helpful&filterByStar=positive&pageNumber='), # 250
+        ('Helpful','?currency=USD&language=en_US&sortBy=helpful&pageNumber=') # 500
+    ]
+
+# 输出txt
+for asin in url_more:
+        for i,j in info:
+                reviews_p1_url.append(base_url+asin+j)
+
+PATH_REVIEWS_URL='E:\\amz_url\\more_url.txt'
+with open(PATH_REVIEWS_URL,'w') as f:
+    for i in tqdm(reviews_p1_url):
+        f.writelines(i+'\n')
